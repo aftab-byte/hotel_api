0c0572bba1b6f770ed9164a809052fd8a5148bdc
aftab-byte <aftabalam710@gmail.com>
2021-07-23 13:48:10 +0530

    added extra column in admin

--- userapi/admin.py
+++ userapi/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from userapi.models import UserDetail ,HotelDetail , Profile
 # Register your models here.
-admin.site.register(UserDetail)
-admin.site.register(HotelDetail)
-admin.site.register(Profile)
+class ItemUserDetail(admin.ModelAdmin):
+    list_display = ("name", "email","phone")
+
+class ItemHotelDetail(admin.ModelAdmin):
+    list_display = ("place", "price")
+
+class ItemProfile(admin.ModelAdmin):
+    list_display = ("phone", "otp")
+    
+admin.site.register(UserDetail ,ItemUserDetail)
+admin.site.register(HotelDetail ,ItemHotelDetail)
+admin.site.register(Profile ,ItemProfile)
