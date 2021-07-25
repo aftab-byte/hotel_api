c87ed18a8b1f4e7e9d30503282edde2a6c7154ed
aftab-byte <aftabalam710@gmail.com>
2021-07-25 17:15:03 +0530

    decode error solved and added homepage

--- flutterapi/urls.py
+++ flutterapi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from userapi import views
 
 urlpatterns = [
+    path('',views.index),
     path('admin/', admin.site.urls),
     path('api/',include('userapi.urls'))
 ]
--- userapi/views.py
+++ userapi/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .models import UserDetail ,Profile ,HotelDetail
 import os
+from django.conf import settings
 from twilio.rest import Client
 import random
 import jwt
@@ -11,23 +12,31 @@ import jwt
 from userapi.serializers import UserDetailSerializer ,HotelDetailSerializer
 # Create your views here.
 otp = random.randint(100000,999999)
-class Register(APIView):
-    def send_otp(self , phone ,otp):
-        print("send_otp is called")
-        # Find your Account SID and Auth Token at twilio.com/console
-        # and set the environment variables. See http://twil.io/secure
-        account_sid = 'ACcd0432425fbd8f4d1e87fb25e9fce7b6'
-        auth_token = '05cc64529a27dd4392d887a81d8e80af'
-        client = Client(account_sid, auth_token)
 
-        message = client.messages \
-                        .create(
-                             body=f'Your login otp is {otp}',
-                             from_='+14352161497',
-                             to='+918400842036'
-                         )
-        print(message.sid)
-        return None
+def index(request):
+    name = settings.ALLOWED_HOSTS[0]
+    context = {
+        "name":name
+    }
+    return render(request,'index.html',context)
+
+class Register(APIView):
+    # def send_otp(self , phone ,otp):
+    #     print("send_otp is called")
+    #     # Find your Account SID and Auth Token at twilio.com/console
+    #     # and set the environment variables. See http://twil.io/secure
+    #     account_sid = 'ACcd0432425fbd8f4d1e87fb25e9fce7b6'
+    #     auth_token = '05cc64529a27dd4392d887a81d8e80af'
+    #     client = Client(account_sid, auth_token)
+    #
+    #     message = client.messages \
+    #                     .create(
+    #                          body=f'Your login otp is {otp}',
+    #                          from_='+14352161497',
+    #                          to='+918400842036'
+    #                      )
+    #     print(message.sid)
+    #     return None
 
     def post(self,request):
         name = request.data['name']
@@ -43,15 +52,13 @@ class Register(APIView):
         serializer = UserDetailSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'status':'true','message':'you are registered succesfully'})
 
         # self.send_otp(phone,otp)
         # profile = Profile(phone = phone ,otp = otp)
         # profile.save()
         # request.session['phone'] = phone
-        #redirect('OtpVerification')
-        else:
-            return Response({'status':'False','message':'your credentials are not valid'})
+        # redirect('OtpVerification')
+        return Response({'status':'true','message':'you are registered succesfully'})
 
 
 
@@ -74,9 +81,11 @@ class OtpVerification(APIView):
                 'phone':profile.phone
             }
 
-            token = jwt.encode(payload ,key = "donottellanyone",algorithm='HS256').decode('utf-8')
+            token = jwt.encode(payload ,key = "donottellanyone",algorithm='HS256')
+            # print(token.decode('utf-8'))
+            # print(type(token))
             response = Response()
-            response.set_cookie(key='jwt',value = token, httponly = True)
+            response.set_cookie(key='jwt',value = token.decode('utf-8'), httponly = True)
             response.data = {
             'status':'True',
             'jwt':token,
